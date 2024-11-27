471e5e033af78b3d581e7f4a765b500b3281338b
Ubuntu <ubuntu@ip-172-31-34-180.ec2.internal>
2024-11-27 08:00:19 +0000

    budget-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
-  
   helper_method :current_user
+  
   private
-    def current_user
-      if session[:login_uid]
-        User.find_by(uid: session[:login_uid])
-      end
+
+  # ログインしているユーザーを取得
+  def current_user
+    if session[:login_uid]
+      User.find_by(uid: session[:login_uid])
     end
+  end
+
+  # ユーザーが認証済みかを確認
+  def authenticate_user
+    unless session[:login_uid]
+      redirect_to login_path, alert: "ログインしてください。"
+    end
+  end
 end
--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,24 +1,43 @@
 class BudgetsController < ApplicationController
-    def index
-        @budgets = Budget.all
-    end
-    
-    def show
-        @budget = Budget.find(params[:id])
-    end
-    
-    def new
-        @budget = Budget.new
-    end
-    
-    def create
-        b = Budget.new(expend: params[:budget][:expend], income: params[:budget][:income])
-        b.save
-        redirect_to root_path
-    end
-    
-    def destroy
-        Budget.find(params[:id]).destroy
-        redirect_to root_path
-    end
+  before_action :authenticate_user
+  before_action :set_budget, only: [:show, :destroy]
+
+  def index
+    @budgets = current_user.budgets
+  end
+
+  def show
+    # @budget は set_budget で取得済み
+  end
+
+  def new
+    @budget = Budget.new
+  end
+
+  def create
+    b = Budget.new(budget_params.merge(
+    user_id: session[:login_uid],
+    expend: params[:budget][:expend],
+    income: params[:budget][:income]
+    ))
+    Rails.logger.debug "Params received: #{params[:budget].inspect}"
+    b.save
+    redirect_to root_path
+  end
+
+  def destroy
+    @budget.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_budget
+    @budget = Budget.find_by(id: params[:id], uid: session[:login_uid])
+    redirect_to root_path unless @budget
+  end
+
+  def budget_params
+    params.require(:budget).permit(:start_time, :expend, :income)
+  end
 end
